501bdcaa6bf516d9dcaca56ac851e2d98a699ca8
Abubakr-Alsheikh <aubakr.5230@gmail.com>
2024-11-01 15:08:40 +0300

    Changing from user folder to home folder

--- core/views.py
+++ core/views.py
@@ -9,9 +9,8 @@ from core.models import Book, Category, EJournal, Report
 def home(request):
     categories = Category.objects.all()
     featured_books = Book.objects.all()[:3]
-    featured_ejournal = EJournal.objects.order_by('-id')[:3] # Assuming 'id' is auto-incrementing, use '-created_at' if you have a datetime field
-    # recent_reports = Report.objects.order_by('-date_published')[:3]
-    return render(request, 'user/home.html', {
+    featured_ejournal = EJournal.objects.order_by('-date_published')[:3]
+    return render(request, 'home/home.html', {
         'categories': categories,
         'featured_books': featured_books, 
         'featured_ejournal': featured_ejournal,
@@ -23,7 +22,7 @@ def search(request):
     query = request.GET.get('q')
 
     if query:
-        # Search in books, e-journals, and reports (using only title and description)
+        # Search in books, e-journals (using only title and description)
         books_results = Book.objects.filter(
             Q(title__icontains=query) | 
             Q(description__icontains=query)
@@ -36,16 +35,16 @@ def search(request):
         # Combine the results into a single list of dictionaries
         results = list(books_results) + list(e_journals_results)
 
-        return render(request, 'user/search_results.html', {'results': results, 'query': query}) # Pass query for display
+        return render(request, 'home/search_results.html', {'results': results, 'query': query}) # Pass query for display
     else:
-        return render(request, 'user/search_results.html', {'query': query}) # No query
+        return render(request, 'home/search_results.html', {'query': query}) # No query
 
 @login_required
 def category_detail(request, pk):
     category = get_object_or_404(Category, pk=pk)
     books = category.books.all()  # Get all books in this category
     e_journals = category.e_journals.all() # Get all e-journals in this category
-    return render(request, 'user/category_detail.html', {
+    return render(request, 'home/category_detail.html', {
         'category': category,
         'books': books,
         'e_journals': e_journals,
@@ -60,7 +59,7 @@ def resources(request):
     for book in books:
         print(book.image.url)
 
-    return render(request, 'user/resources.html', {'combined_resources': combined_resources})
+    return render(request, 'home/resources.html', {'combined_resources': combined_resources})
 
 @login_required
 def resource_detail(request, resource_type,  pk):
@@ -87,7 +86,7 @@ def resource_detail(request, resource_type,  pk):
         resource_type = 'unknown'
         additional_fields = {}
 
-    return render(request, 'user/resource_detail.html', {
+    return render(request, 'home/resource_detail.html', {
         'resource': resource,
         'resource_type': resource_type,
         **additional_fields
@@ -96,12 +95,12 @@ def resource_detail(request, resource_type,  pk):
 @login_required
 def reports(request):
     all_reports = Report.objects.order_by('-date_published') # Sort by date_published descending
-    return render(request, 'user/reports.html', {'all_reports': all_reports})
+    return render(request, 'home/reports.html', {'all_reports': all_reports})
 
 @login_required
 def report_detail(request, pk):
     report = get_object_or_404(Report, pk=pk)
-    return render(request, 'user/report_detail.html', {'report': report})
+    return render(request, 'home/report_detail.html', {'report': report})
 
 @login_required
 def notifications(request):
